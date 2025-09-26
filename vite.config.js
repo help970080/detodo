683becab0bc667cf8e422a180d4f50d250e3c4b6
Leonardo Luna <consultorpremium764@gmail.com>
2025-09-26 01:13:50 -0600

    vamos bien

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Indica que el root es la carpeta actual (frontend)
+  root: '.',
   build: {
     outDir: "dist"
   },
   server: {
     port: 3000
+  },
+  // SOLUCIÓN FINAL: Definir la variable directamente para producción
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify('https://detodounpocobackend.onrender.com')
   }
 });
